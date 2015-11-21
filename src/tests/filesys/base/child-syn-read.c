cbed0597dbdbbbc617de84bb506322926047a298
rhs0266 <dkflfkdeh@naver.com>
2015-11-22 01:45:15 +0900

    Fix changed test code

--- src/tests/filesys/base/child-syn-read.c
+++ src/tests/filesys/base/child-syn-read.c
@@ -38,7 +38,7 @@ main (int argc, const char *argv[])
   CHECK ((fd = open (file_name)) > 1, "open \"%s\"", file_name);
   for (i = 0; i < sizeof buf; i++) 
     {
-		msg("os\n");
+        //msg("os\n");
       char c;
       CHECK (read (fd, &c, 1) > 0, "read \"%s\"", file_name);
       compare_bytes (&c, buf + i, 1, i, file_name);
